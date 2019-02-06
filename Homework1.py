4a66e6b44427e3a5017a7c1253e010512e617725
Iris <Irisdyrmishi@gmail.com>
2019-02-06 22:56:24 +0200

    Exercise1. Finished

--- Homework1.py
+++ Homework1.py
@@ -1,23 +1,28 @@
-num1 = input("Input the first number  ")
-num2 = input("Input the second number  ") 
-action = input("What would you like to do? 1.Add 2.Substract 3.Divide 4. Multiplication")
-
 
 def adding(x,y):
     return x + y
+
 def substracting(x,y):
     return x - y
+
 def divide(x,y):
     return x / y
+
 def multiplication(x, y):
     return x * y
 
+num1 = int(input("Input the first number  "))
+num2 = int(input("Input the second number  "))
+acti = int(input("What would you like to do? 1.Add 2.Substract 3.Divide 4. Multiplication "))
 
-if action == 1:
-    print(adding(num1,num2)
-elif action == 2:
-    print(bsubstracting(num1, num2))
-elif action == 3:
-    print(divide(num1,num2))
+if acti == 1:
+   print(num1,"+",num2,"=", adding(num1,num2))
+
+elif acti == 2:
+   print(num1,"-",num2,"=", substracting(num1,num2))
+
+elif acti == 3:
+   print(num1,"/",num2,"=", divide(num1,num2))
+
 else:
-    print(multiplication(num1,num2))
+   print(num1,"*",num2,"=", multiplication(num1,num2))
